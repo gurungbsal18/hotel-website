8d3bb3f20f70395f2600f17299765732dfbce331
gurungbsal18 <gurungbsal18@gmail.com>
2025-03-09 17:34:33 +0545

    favicon added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
-import Image from "next/image";
-import { HotelRoomData } from "./api/RoomData";
-import { IoBedOutline } from "react-icons/io5";
-import { PiBathtub } from "react-icons/pi";
-import { IoWifiSharp } from "react-icons/io5";
-import HomePageCheckInSection from "@/components/HomePageCheckInSection";
 import HomePage from "@/components/HomePage/HomePage";
 
 export default function Home() {
