9b0b13a27f1fc0201e57b3f3b0703ec3f6bcfbad
gurungbsal18 <gurungbsal18@gmail.com>
2025-03-09 17:28:26 +0545

    footer responsive created

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,23 +10,23 @@ export default function Footer() {
   return (
     <>
       <div className="bg-primary flex flex-col justify-center items-center gap-8 text-white py-16 mt-16">
-        <div className="flex flex-col gap-2 justify-center items-center w-3/6">
+        <div className="flex flex-col gap-2 justify-center items-center w-full md:w-3/4 lg:w-3/6">
           <Image src="/Hotel-logo.svg" alt="hotel" width={100} height={100} />
           <p className="text-center">
             Welcome to our exquisite Hotel, where relaxation meets luxury, and
             every moment is a seaside dream come true.
           </p>
         </div>
-        <div className="grid grid-cols-12 w-full">
-          <span className="col-span-4 flex flex-col justify-center items-center gap-1">
+        <div className="grid grid-cols-1 md:grid-cols-12 w-full gap-8">
+          <span className="col-span-1 md:col-span-4 flex flex-col justify-center items-center gap-1">
             <p className="font-bold">Address</p>
             <p>Kathmandu, Nepal</p>
           </span>
-          <span className="col-span-4 flex flex-col justify-center items-center gap-1">
+          <span className="col-span-1 md:col-span-4 flex flex-col justify-center items-center gap-1">
             <p className="font-bold">Contact</p>
             <p>+977 1234567890</p>
           </span>
-          <span className="col-span-4 flex flex-col justify-center items-center gap-1">
+          <span className="col-span-1 md:col-span-4 flex flex-col justify-center items-center gap-1">
             <p className="font-bold">Follow</p>
             <span className="flex gap-2 items-center">
               <FaFacebook />
